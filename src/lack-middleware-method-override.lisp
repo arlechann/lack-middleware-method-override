98355560168a7c37f6b4bf15abc13a8460b1437b
arlechann <dragnov3728@gmail.com>
2020-04-24 01:49:14 +0900

    Add split function test

--- src/lack-middleware-method-override.lisp
+++ src/lack-middleware-method-override.lisp
@@ -4,4 +4,6 @@
             (:export method-override))
 (in-package :lack-middleware-method-override)
 
+(defun split ())
+
 (defun method-override ())
--- test/lack-middleware-method-override.lisp
+++ test/lack-middleware-method-override.lisp
@@ -1,6 +1,14 @@
 (in-package :cl-user)
 (defpackage :lack-middleware-method-override-test
-            (:use :cl :prove))
+            (:use :cl
+                  :prove
+                  :lack-middleware-method-override))
 (in-package :lack-middleware-method-override-test)
 
+(plan 4)
+
+(is (lack-middleware-method-override::split "=" "a=b") '("a" "b") :test #'equalp)
+(is (lack-middleware-method-override::split "=" "a") '("a") :test #'equalp)
+(is (lack-middleware-method-override::split "" "a=b") '("a" "=" "b") :test #'equalp)
+(is (lack-middleware-method-override::split "cd" "abcdefabcdef") '("ab" "efab" "ef") :test #'equalp)
 (finalize)
