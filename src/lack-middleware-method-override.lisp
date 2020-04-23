25c4faac2bf69cd761353954d4e13eed1d370376
arlechann <dragnov3728@gmail.com>
2020-04-24 01:49:41 +0900

    Add splite function

--- src/lack-middleware-method-override.lisp
+++ src/lack-middleware-method-override.lisp
@@ -4,6 +4,13 @@
             (:export method-override))
 (in-package :lack-middleware-method-override)
 
-(defun split ())
+(defun split (sep str &optional (acc nil))
+  (if (string= sep "")
+      (map 'list #'string str)
+      (let ((pos (search sep str))
+            (len (length sep)))
+        (if (null pos)
+            (reverse (cons str acc))
+            (split sep (subseq str (+ pos len)) (cons (subseq str 0 pos) acc))))))
 
 (defun method-override ())
