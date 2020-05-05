147006e9294c509b8467efbc0c585cb277ecf845
arlechann <dragnov3728@gmail.com>
2020-05-06 03:32:43 +0900

    Rewrite almost

--- src/lack-middleware-method-override.lisp
+++ src/lack-middleware-method-override.lisp
@@ -4,34 +4,10 @@
             (:export *method-override*))
 (in-package :lack-middleware-method-override)
 
-(defun split (sep str)
-  (labels ((rec (sep str acc)
-            (let ((pos (search sep str))
-                  (len (length sep)))
-              (if (null pos)
-                  (reverse (cons str acc))
-                  (rec sep (subseq str (+ pos len)) (cons (subseq str 0 pos) acc))))))
-    (if (or (= (length sep) 0)
-            (= (length str) 0))
-        (map 'list #'string str)
-        (rec sep str nil))))
-
-(defun parse-query (query)
-  (mapcar (lambda (pair) (cons (car pair) (cadr pair)))
-          (mapcar (lambda (str) (split "=" str))
-                  (split "&" query))))
-
 (defun get-method (parsed)
   (cdr (assoc "_method" parsed :test #'string=)))
 
-(defun request-handler (env)
-  (let ((current (getf env :request-method))
-        (rewrited (get-method (parse-query (getf env :query-string)))))
-    (if (and (eq current :POST)
-              rewrited)
-        (progn (setf (getf env :request-method) (intern rewrited :keyword))
-               env)
-        env)))
+(defun request-handler (env))
 
 (defvar *method-override*
   (lambda (app)
--- test/lack-middleware-method-override.lisp
+++ test/lack-middleware-method-override.lisp
@@ -7,83 +7,9 @@
 
 (plan 15)
 
-(is (lack-middleware-method-override::split "=" "a=b") '("a" "b") :test #'equalp)
-(is (lack-middleware-method-override::split "=" "a") '("a") :test #'equalp)
-(is (lack-middleware-method-override::split "" "a=b") '("a" "=" "b") :test #'equalp)
-(is (lack-middleware-method-override::split "cd" "abcdefabcdef") '("ab" "efab" "ef") :test #'equalp)
-(is (lack-middleware-method-override::split "=" "") nil)
-
-(is (lack-middleware-method-override::parse-query "a=b") '(("a" . "b")) :test #'equalp)
-(is (lack-middleware-method-override::parse-query "a=b&c=d") '(("a" . "b") ("c" . "d")) :test #'equalp)
-(is (lack-middleware-method-override::parse-query "") nil)
-(is (lack-middleware-method-override::parse-query "ab=cd&ef=ab&cd=ef") '(("ab" . "cd") ("ef" . "ab") ("cd" . "ef")) :test #'equalp)
-
 (is (lack-middleware-method-override::get-method '(("a" . "b") ("c" . "d"))) nil)
 (is (lack-middleware-method-override::get-method '(("_method" . "POST") ("c" . "d"))) "POST" :test #'equalp)
 (is (lack-middleware-method-override::get-method '(("_method" . "POST") ("_method" . "DELETE"))) "POST" :test #'equalp)
 (is (lack-middleware-method-override::get-method nil) nil)
-
-(is (lack-middleware-method-override::request-handler
-      '(:REQUEST-METHOD :POST
-        :SCRIPT-NAME ""
-        :PATH-INFO "/"
-        :SERVER-NAME "localhost"
-        :SERVER-PORT 5000
-        :SERVER-PROTOCOL :HTTP/1.1
-        :REQUEST-URI "/?_method=PUT"
-        :URL-SCHEME "http"
-        :REMOTE-ADDR "127.0.0.1"
-        :REMOTE-PORT 49942
-        :QUERY-STRING "_method=PUT"
-        :CONTENT-LENGTH NIL
-        :CONTENT-TYPE NIL
-        :CLACK.STREAMING T
-        :CLACK.IO))
-    '(:REQUEST-METHOD :PUT
-      :SCRIPT-NAME ""
-      :PATH-INFO "/"
-      :SERVER-NAME "localhost"
-      :SERVER-PORT 5000
-      :SERVER-PROTOCOL :HTTP/1.1
-      :REQUEST-URI "/?_method=PUT"
-      :URL-SCHEME "http"
-      :REMOTE-ADDR "127.0.0.1"
-      :REMOTE-PORT 49942
-      :QUERY-STRING "_method=PUT"
-      :CONTENT-LENGTH NIL
-      :CONTENT-TYPE NIL
-      :CLACK.STREAMING T
-      :CLACK.IO))
-(is (lack-middleware-method-override::request-handler
-      '(:REQUEST-METHOD :POST
-        :SCRIPT-NAME ""
-        :PATH-INFO "/"
-        :SERVER-NAME "localhost"
-        :SERVER-PORT 5000
-        :SERVER-PROTOCOL :HTTP/1.1
-        :REQUEST-URI "/?_method=DELETE&q=abc"
-        :URL-SCHEME "http"
-        :REMOTE-ADDR "127.0.0.1"
-        :REMOTE-PORT 49942
-        :QUERY-STRING "_method=DELETE&q=abc"
-        :CONTENT-LENGTH NIL
-        :CONTENT-TYPE NIL
-        :CLACK.STREAMING T
-        :CLACK.IO))
-    '(:REQUEST-METHOD :DELETE
-      :SCRIPT-NAME ""
-      :PATH-INFO "/"
-      :SERVER-NAME "localhost"
-      :SERVER-PORT 5000
-      :SERVER-PROTOCOL :HTTP/1.1
-      :REQUEST-URI "/?_method=DELETE&q=abc"
-      :URL-SCHEME "http"
-      :REMOTE-ADDR "127.0.0.1"
-      :REMOTE-PORT 49942
-      :QUERY-STRING "_method=DELETE&q=abc"
-      :CONTENT-LENGTH NIL
-      :CONTENT-TYPE NIL
-      :CLACK.STREAMING T
-      :CLACK.IO))
 
 (finalize)
